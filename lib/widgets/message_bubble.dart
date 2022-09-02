c9ea4fcb90ecaa891e02063e52ea0135209b48ad
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-09-02 00:48:49 -0400

    clean up styles

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   final String text;
   final String sender;
+  final bool isAuthor;
 
-  const MessageBubble(this.text, this.sender);
+  const MessageBubble({this.text, this.sender, this.isAuthor});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isAuthor ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(
             sender,
@@ -21,15 +23,20 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(isAuthor ? 30 : 0),
+              topRight: Radius.circular(isAuthor ? 0 : 30),
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
             elevation: 5,
-            color: Colors.lightBlueAccent,
+            color: isAuthor ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               child: Text(
                 text,
                 style: TextStyle(
-                  color: Colors.white,
+                  color: isAuthor ? Colors.white : Colors.black,
                   fontSize: 15,
                 ),
               ),
--- lib/widgets/message_stream.dart
+++ lib/widgets/message_stream.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/widgets/message_bubble.dart';
 import 'package:flutter/material.dart';
 
 class MessageStream extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
 
   @override
@@ -24,11 +26,16 @@ class MessageStream extends StatelessWidget {
           final text = messageData['text'];
           final sender = messageData['sender'];
 
-          return MessageBubble(text, sender);
+          return MessageBubble(
+            text: text,
+            sender: sender,
+            isAuthor: sender == _auth.currentUser.email,
+          );
         }).toList();
 
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             children: messageBubbles,
           ),
